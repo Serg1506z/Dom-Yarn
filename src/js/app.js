33dbfc5a30a1385e1d3e83581c3857e77265b72b
Serg1506z <136631034+Serg1506z@users.noreply.github.com>
2024-07-10 18:19:49 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,32 @@
 import goblin from '../pic/goblin.png'
 const containerElements = document.querySelectorAll('.place__container')
+const countWinView = document.querySelector('.countWin')
+const countOverView = document.querySelector('.countOver')
 let num 
 const condition = true
 let count = 0
+const arrCount = []
+let countWin = 0
 
 
 function handleClick(e){
     const place = e.target
-    console.log(place);
-    // const goblinElement = .querySelector('img')
-    // console.log(goblinElement);
-    // if(goblinElement){
-    //     goblinElement.remove()
-    //     console.log('Kill the goblin');
-    // }else {
-    //     console.log('you Missing');
-    //     count++
-    //     if(count >= 5){
-    //         alert('Game over')
-    //     }
-    // }
+    const goblinElement = place.querySelector('img') || place.tagName === 'IMG' ? place : null
+    if(goblinElement){
+        countWin++
+        countWinView.textContent = countWin
+        arrCount[count - 1] = true
+        goblinElement.remove()
+        console.log('Kill the goblin');
+    } else {
+        console.log('you Missing');
+        arrCount[count - 1] = false
+        countOverView.textContent = 5 - arrCount.filter(item => item === false).length
+    }
   
 }
 
-setInterval(() => {
+const intervalVisible = setInterval(() => {
     while(condition){
         const random = Math.floor(Math.random() * (4 - 1 + 1)) + 1
         if(random !== num) {
@@ -36,8 +39,20 @@ setInterval(() => {
     containerElements[num - 1].append(img)
 }, 1000)
 
-setInterval(() => {
+const IntervalShow = setInterval(() => {
+    arrCount.push(null)
+    console.log(arrCount, count);
+    if(arrCount[count - 1] === null){
+        arrCount[count - 1] = false
+    }
     containerElements.forEach(item => item.innerHTML = '')
+    countOverView.textContent = 5 - arrCount.filter(item => item === false).length
+    if(arrCount.filter(item => item === false).length >= 5){
+        alert('Game over')
+        clearInterval(intervalVisible)
+        clearInterval(IntervalShow)
+    }
+    count++
 }, 2000)
 
 
